13cf86025a3c35f8b492d29a3e500023887ca372
Rory N <rorynickolls@gmail.com>
2021-12-21 16:34:11 +0000

    Rotating cube

---
+++ src/main.rs
@@ -0,0 +1,137 @@
+#[macro_use]
+extern crate glium;
+use glium::Surface;
+
+mod shapes;
+use shapes::square;
+
+fn main() {
+    use glium::glutin;
+
+    let mut event_loop = glutin::event_loop::EventLoop::new();
+    let wb = glutin::window::WindowBuilder::new();
+    let cb = glutin::ContextBuilder::new().with_depth_buffer(24);
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+
+    let mut t: f32 = 0.0;
+    event_loop.run(move |ev, _, control_flow| {
+        let next_frame_time =
+            std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+        t += 0.002;
+
+        // Handle window events
+        match ev {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                glutin::event::WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                }
+                _ => return,
+            },
+            _ => (),
+        }
+
+        let shape = shapes::cube();
+        let (vertex_buffer, normal_buffer, index_buffer) = (
+            glium::VertexBuffer::new(&display, &shape.vertices).unwrap(),
+            glium::VertexBuffer::new(&display, &shape.normals).unwrap(),
+            glium::IndexBuffer::new(
+                &display,
+                glium::index::PrimitiveType::TrianglesList,
+                &shape.indices,
+            )
+            .unwrap(),
+        );
+
+        let vertex_shader_src = r#"
+        #version 150
+
+        in vec3 position;
+        in vec3 normal;
+
+        out vec3 v_normal;
+
+        uniform mat4 perspective;
+        uniform mat4 matrix;
+
+        void main() {
+            v_normal = transpose(inverse(mat3(matrix))) * normal;
+            gl_Position = perspective * matrix * vec4(position, 1.0);
+        }
+        "#;
+
+        let fragment_shader_src = r#"
+        #version 140
+
+        in vec3 v_normal;
+        out vec4 color;
+
+        uniform vec3 u_light;
+
+        void main() {
+            float brightness = dot(normalize(v_normal), normalize(u_light));
+            vec3 dark = vec3(0.6, 0.0, 0.0);
+            vec3 regular = vec3(1.0, 0.0, 0.0);
+            color = vec4(mix(dark, regular, brightness), 1.0);
+        }
+        "#;
+
+        let program =
+            glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None)
+                .unwrap();
+
+        // Start drawing on window
+        let mut target = display.draw();
+        target.clear_color_and_depth((0.01, 0.01, 0.01, 1.0), 1.0);
+
+        let perspective = {
+            let (width, height) = target.get_dimensions();
+            let aspect_ratio = height as f32 / width as f32;
+
+            let fov: f32 = 3.141592 / 3.0;
+            let zfar = 1024.0;
+            let znear = 0.1;
+
+            let f = 1.0 / (fov / 2.0).tan();
+
+            [
+                [f * aspect_ratio, 0.0, 0.0, 0.0],
+                [0.0, f, 0.0, 0.0],
+                [0.0, 0.0, (zfar + znear) / (zfar - znear), 1.0],
+                [0.0, 0.0, -(2.0 * zfar * znear) / (zfar - znear), 0.0],
+            ]
+        };
+
+        let uniforms = uniform! {
+            matrix: [
+                [t.cos(), 0.0, t.sin(), 0.0],
+                [0.0, -1.0, 0.0, 0.0],
+                [-t.sin(), 0.0, t.cos(), 0.0],
+                [0.0, 0.0, 2.0, 1.0f32],
+            ],
+            u_light: [-1.0, 0.4, 0.9f32],
+            perspective: perspective
+        };
+
+        let params = glium::DrawParameters {
+            depth: glium::Depth {
+                test: glium::draw_parameters::DepthTest::IfLess,
+                write: true,
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+
+        target
+            .draw(
+                (&vertex_buffer, &normal_buffer),
+                &index_buffer,
+                &program,
+                &uniforms,
+                &params,
+            )
+            .unwrap();
+        target.finish().unwrap();
+    });
+}
